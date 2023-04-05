f18e2aa49bbb1ec0a45a42b8e1520425f736a413
Dary <dady281100@gmail.com>
2023-04-04 23:25:29 -0500

    Adding an extra card

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -99,6 +99,18 @@ return (
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
+            
+            <Card style={{ width: '18rem', margin: '8px' }}>
+                <Card.Img variant="top" src="https://via.placeholder.com/400x400" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
 
         </Row>
     </Container>
