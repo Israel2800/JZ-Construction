f5fc1b8f911158b3551ad3a4b070ffb347196da9
Israel Aguilar <dady281100@gmail.com>
2023-05-27 23:49:49 -0500

    updated data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -9,12 +9,12 @@ import service7 from './assets/images/service7.jpeg';
 
 
 export const services = [
-    {
-        image: service1,
-        title: "CONSTRUCTION 1",
-        description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        button: "link to page"
-    },
+    // {
+    //     image: service1,
+    //     title: "CONSTRUCTION 1",
+    //     description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    //     button: "link to page"
+    // },
     {
         image: service1,
         title: "CONSTRUCTION 2",
