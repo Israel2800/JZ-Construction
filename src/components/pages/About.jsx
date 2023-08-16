af4fe4a2961d324f460a3b8bbdb34e23f3886110
Israel Aguilar <dady281100@gmail.com>
2023-08-15 22:40:54 -0600

    Update About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,9 @@
-/*import React from 'react';
+import React from 'react';
 
 export default function About({ HandlePageChange }) {
     return(
         <section>
-            
+            Testing...
         </section>
     );
 }
-*/
